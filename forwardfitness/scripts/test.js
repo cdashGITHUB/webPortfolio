fc6d09a2dd1410638216f7611c85c5c4ff491c10
cdashGITHUB <114946247+cdashGITHUB@users.noreply.github.com>
2023-02-13 18:33:36 +0000

    actually added forward fitness

---
+++ forwardfitness/scripts/test.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function day() {
+	var day = document.getElementById("date");
+	if (day === "Saturday") {
+		document.write("I love Saturdays");
+	} else {
+		documnet.write("I wish it was Saturday");
+	}
+}
